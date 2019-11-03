7cb69970c3769c9d423d089229664ab40ed45839
lulivi <luislivilla@gmail.com>
2019-11-04 00:14:56 +0100

    Set up travis cofiguration file and tests
    Finished travis environment configuration and documentation tests set-up.

--- tasks/docs.py
+++ tasks/docs.py
@@ -1,39 +1,24 @@
 import sys
+import tempfile
 
-from invoke import task
+from invoke import task, run
 from pathlib import Path
 from sultan.api import Sultan
 
-REPO_ROOT = Path(__file__).parents[1]
-default_report_name = "proyecto"
+ROOT = Path(__file__).parents[1].resolve()
+try:
+    ASPELL_DIR_PATH = ROOT.joinpath("docs", "aspell").resolve(strict=True)
+    REPORT_DIR_PATH = ROOT.joinpath("docs", "report").resolve(strict=True)
+    FILTER_DIR_PATH = ROOT.joinpath("docs", "filters").resolve(strict=True)
+except FileNotFoundError as e:
+    sys.exit("{}: {}".format(e.strerror, e.filename))
+REPORT_NAME = "proyecto"
 
 
 @task
-def update_word_list(c, extra_word_list_file="extra_words", ext="txt"):
-    """Update aspell extra dictionary from word list."""
-    print("Updating aspell extra words list...")
-    try:
-        aspell_dicts_path = REPO_ROOT.joinpath("docs", "aspell").resolve()
-        extra_word_list_path = aspell_dicts_path.joinpath(
-            "{}.{}".format(extra_word_list_file, ext)
-        ).resolve()
-    except FileNotFoundError as e:
-        sys.exit("Error: {}".format(e))
-
-    aspell_extra_dict_path = aspell_dicts_path.joinpath(
-        "{}.rws".format(extra_word_list_file)
-    )
-
-    with Sultan.load(cwd=str(aspell_dicts_path)) as s:
-        s.cat(str(extra_word_list_path)).pipe().aspell(
-            "--lang=es", "create", "master", str(aspell_extra_dict_path)
-        ).run(quiet=True)
-
-
-@task
-def clean(c, report_name=default_report_name):
+def clean(c):
     """Clean doc construction."""
-    print("Cleaning ...")
+    print("Cleaning latex and pweave files...")
     patterns = (
         "*.pyc",
         "docs/report/_*",
@@ -41,61 +26,74 @@ def clean(c, report_name=default_report_name):
         "docs/report/*.pyc",
         "docs/report/*.bbl",
         "docs/report/*.blg",
-        "docs/report/secciones/*.tex",
-        "docs/report/{}.aux".format(report_name),
-        "docs/report/{}.log".format(report_name),
-        "docs/report/{}.out".format(report_name),
-        "docs/report/{}.lof".format(report_name),
-        "docs/report/{}.lot".format(report_name),
-        "docs/report/{}.toc".format(report_name),
-        "docs/report/{}.fls".format(report_name),
-        "docs/report/{}.fdb_latexmk".format(report_name),
-        "docs/report/{}.pdf".format(report_name),
-        "docs/report/{}/".format(report_name),
+        "docs/report/seccionges/*.tex",
+        "docs/report/{}.aux".format(REPORT_NAME),
+        "docs/report/{}.log".format(REPORT_NAME),
+        "docs/report/{}.out".format(REPORT_NAME),
+        "docs/report/{}.lof".format(REPORT_NAME),
+        "docs/report/{}.lot".format(REPORT_NAME),
+        "docs/report/{}.toc".format(REPORT_NAME),
+        "docs/report/{}.fls".format(REPORT_NAME),
+        "docs/report/{}.fdb_latexmk".format(REPORT_NAME),
+        "docs/report/{}.pdf".format(REPORT_NAME),
+        "docs/report/{}/".format(REPORT_NAME),
     )
 
-    with Sultan.load() as s:
+    with Sultan.load(cwd=str(REPORT_DIR_PATH)) as s:
         for pattern in patterns:
-            s.rm("-rf {}".format(pattern)).run(quiet=True)
+            s.rm("-rf", pattern)
 
 
 @task(clean)
 def latex(c):
     """Create tex file."""
-    print("Building latex file and figures through pweave ...")
-    report_dir_path = Path("docs", "report")
-
-    try:
-        report_dir_path = report_dir_path.resolve()
-
-        if not report_dir_path.exists():
-            raise FileNotFoundError
-    except FileNotFoundError:
-        sys.exit("{} not found.".format(str(report_dir_path)))
-
-    with Sultan.load() as s:
-        for pweave_file in report_dir_path.glob("**/*.texw"):
+    print("Building latex files and figures through pweave ...")
+    fig_dir = str(REPORT_DIR_PATH.joinpath("figures"))
+    with Sultan.load(cwd=ROOT) as s:
+        for pweave_file in REPORT_DIR_PATH.glob("**/*.texw"):
             tex_file = str(pweave_file.with_suffix(".tex"))
             s.pweave(
                 "--format=texminted",
                 "--documentation-mode",
-                "--figure-directory={}".format(
-                    str(report_dir_path.joinpath("figures"))
-                ),
+                "--figure-directory={}".format(fig_dir),
                 "--output={}".format(tex_file),
                 "{}".format(str(pweave_file)),
             ).run(quiet=True)
 
 
 @task(latex)
-def pdf(c, report_name=default_report_name, bibtex=True):
+def plain(c):
+    """Create plain documentation from latex."""
+    print("Creating plain documentation from latex ...")
+    plain_report_path = Path(tempfile.gettempdir()).joinpath(
+        "plain_report.txt"
+    )
+    filters_path = FILTER_DIR_PATH.joinpath("filter.py")
+    pandoc_args = [
+        str(REPORT_DIR_PATH.joinpath("{}.tex".format(REPORT_NAME))),
+        "--output={}".format(plain_report_path),
+        "--from=latex",
+        "--to=plain",
+    ]
+
+    if filters_path.resolve().exists():
+        pandoc_args.append("--filter={}".format(str(filters_path)))
+
+    with Sultan.load(cwd=str(REPORT_DIR_PATH)) as s:
+        s.pandoc(*pandoc_args).run(quiet=True)
+
+
+@task(latex)
+def pdf(c, bibtex=True):
     """Create pdf file."""
     print("Building pdf through pdflatex ...")
-    pdflatex_arg = "-shell-escape {}.tex".format(report_name)
-    with Sultan.load(cwd="docs/report") as s:
+    pdflatex_arg = "-shell-escape {}.tex".format(REPORT_NAME)
+    with Sultan.load(cwd=str(REPORT_DIR_PATH), logging=False) as s:
         s.pdflatex(pdflatex_arg).run(quiet=True)
-        if bibtex:
-            s.bibtex("{}.aux".format(report_name)).run(quiet=True)
+        # Ignore error if there are no citations
+        s.bibtex("{}.aux".format(REPORT_NAME)).run(
+            quiet=True, halt_on_nonzero=False
+        )
         s.pdflatex(pdflatex_arg).run(quiet=True)
         s.pdflatex(pdflatex_arg).run(quiet=True)
 
