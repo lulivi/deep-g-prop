a57e0c1f88015acd2e2817fa38c6e23cf57926aa
lulivi <luislivilla@gmail.com>
2019-11-13 23:47:19 +0100

    Removed unused stuff

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,10 +1,5 @@
-from invoke import task, Collection
+from invoke import Collection
 
 from . import docs, tests
 
-
-#ns = Collection()
-#ns.add_collection(docs)
-#ns.add_collection(tests)
 ns = Collection(docs, tests)
-
