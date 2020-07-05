b64054c43662eefb57bf21b336607ef32f4c22d5
lulivi <luislivilla@gmail.com>
2020-07-06 00:02:56 +0200

    Create proben1 partitions for spambase
    Closes #71

---
+++ src/dataset_to_proben1.py
@@ -0,0 +1,69 @@
+"""Obtain proben1-like partitions."""
+from pathlib import Path
+
+import click
+import numpy as np
+import pandas as pd
+
+from src.common import SEED
+
+
+@click.command()
+@click.argument(
+    "file_path", type=Path,
+)
+def cli(file_path: Path):
+    """Split a dataset into 3 proben1-like partitions.
+
+    Split the dataset given by ``FILE_PATH`` into 3 partitions of train (trn),
+    validation (val) and test (tst).
+
+    """
+    if not file_path.exists() or not file_path.is_file():
+        raise click.BadParameter(
+            (
+                "Dataset path provided does not exists or it is not a file "
+                f"'{str(file_path)}'."
+            ),
+            param_hint="FILE_PATH",
+        )
+
+    dataset_path = file_path.resolve()
+    dataset_directory = dataset_path.parent
+    dataset_name = dataset_path.stem
+
+    dataframe = pd.read_csv(str(file_path), dtype=str)
+    header = ",".join(list(dataframe.columns))
+
+    for partition in range(1, 4):
+        # pylint: disable=unbalanced-tuple-unpacking
+        trn, val, tst = np.split(
+            dataframe.sample(frac=1, random_state=SEED),
+            [int(0.5 * len(dataframe)), int(0.75 * len(dataframe))],
+        )
+        np.savetxt(
+            str(dataset_directory / f"{dataset_name}{partition}.trn"),
+            trn,
+            fmt="%s",
+            delimiter=",",
+            header=header,
+        )
+        np.savetxt(
+            str(dataset_directory / f"{dataset_name}{partition}.val"),
+            val,
+            fmt="%s",
+            delimiter=",",
+            header=header,
+        )
+        np.savetxt(
+            str(dataset_directory / f"{dataset_name}{partition}.tst"),
+            tst,
+            fmt="%s",
+            delimiter=",",
+            header=header,
+        )
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    cli()
---
+++ tests/src/test_dataset_to_proben1.py
@@ -0,0 +1,39 @@
+"""Test the :mod:`src.dataset_to_proben1` module."""
+import unittest
+
+from pathlib import Path
+from unittest import mock
+
+from click.testing import CliRunner
+
+from settings import PROBEN1_DIR_PATH
+from src.dataset_to_proben1 import cli
+
+
+class TestDatasetToProben1(unittest.TestCase):
+    """Tests for the dataset to proben1 converter CLI."""
+
+    def test_cli_dataset_not_found(self):
+        """Dataset path could not be found."""
+        file_path = Path("non/existent/path")
+        runner = CliRunner()
+        result = runner.invoke(cli, [str(file_path)])
+
+        self.assertEqual(result.exit_code, 2, result.stdout)
+
+    @mock.patch("src.dataset_to_proben1.np")
+    def test_cli_ok(self, mock_np):
+        """Dataset is found."""
+        mock_np.split.return_value = ("train", "validation", "test")
+        runner = CliRunner()
+        with mock.patch("src.dataset_to_proben1.pd"):
+            result = runner.invoke(
+                cli, [str(PROBEN1_DIR_PATH / "spambase.csv")]
+            )
+        print(str(PROBEN1_DIR_PATH / "spambase.csv"))
+
+        self.assertEqual(result.exit_code, 0, result.stdout)
+
+
+if __name__ == "__main__":
+    unittest.main()
